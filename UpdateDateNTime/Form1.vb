53b63cd914ee4f0f36a30258b8d399813c1d22e3
notestudios <joaovodias@gmail.com>
2023-06-18 12:13:51 -0300

    command update

--- UpdateDateNTime/Form1.vb
+++ UpdateDateNTime/Form1.vb
@@ -4,39 +4,16 @@
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         ' "Update!" Button
-        Dim startInfo As New ProcessStartInfo()
-        startInfo.FileName = "cmd.exe"
-        startInfo.Verb = "runas" ' Admin request (not working for some reason)
+        Dim command As String
+        command = "initialize.cmd" & " & " & "startTimeService.cmd" & " & " & "resync.cmd"
 
-        Dim cmdsPath As String = "cmds\"
-        Dim file1 As String = cmdsPath + "intialize.cmd"
-        Dim file2 As String = cmdsPath + "start.cmd"
-        Dim file3 As String = cmdsPath + "resync.cmd"
-
-        Try
-            ' Load and Run .cmd files
-            startInfo.Arguments = "/c " & file1
-            Dim process1 As Process = Process.Start(startInfo)
-            process1.WaitForExit()
-
-            startInfo.Arguments = "/c " & file2
-            Dim process2 As Process = Process.Start(startInfo)
-            process2.WaitForExit()
-
-            startInfo.Arguments = "/c " & file3
-            Dim process3 As Process = Process.Start(startInfo)
-            process3.WaitForExit()
-
-            MessageBox.Show("Arquivos .cmd executados com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        Catch ex As Exception
-            MessageBox.Show("Erro ao executar os arquivos .cmd: " & ex.Message, "An error occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
+        Shell("cmd.exe /c " & command)
     End Sub
-    ' TODO: Restart w32time button
+    ' TODO: Restart w32time button (hidden)
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
-    ' TODO: Repair w32time button
+    ' TODO: Repair w32time button (hidden)
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
 
     End Sub
