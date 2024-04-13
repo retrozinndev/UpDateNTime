ad3fc39eeaa3aabd6fc4e8e1c3f9c7c9b1ed28ca
Joao Dias <joaovodias@gmail.com>
2024-04-13 14:10:03 -0300

    :sparkles: feat: start minimized when autostart is on

--- UpdateDateNTime/Window.Designer.vb
+++ UpdateDateNTime/Window.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Window
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Window))
         PictureBox1 = New PictureBox()
         Label2 = New Label()
@@ -31,6 +32,7 @@ Partial Class Window
         Label4 = New Label()
         githubLink = New LinkLabel()
         githubLogo = New PictureBox()
+        NotifyIcon = New NotifyIcon(components)
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         CType(githubLogo, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
@@ -38,55 +40,98 @@ Partial Class Window
         ' PictureBox1
         ' 
         PictureBox1.ErrorImage = My.Resources.Resources._error
-        resources.ApplyResources(PictureBox1, "PictureBox1")
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), Image)
+        PictureBox1.Location = New Point(218, 39)
         PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(204, 104)
+        PictureBox1.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBox1.TabIndex = 1
         PictureBox1.TabStop = False
         ' 
         ' Label2
         ' 
-        resources.ApplyResources(Label2, "Label2")
+        Label2.AutoSize = True
+        Label2.Location = New Point(157, 177)
         Label2.Name = "Label2"
+        Label2.Size = New Size(327, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Update your date and time just by clicking the button below!"
         ' 
         ' Label3
         ' 
-        resources.ApplyResources(Label3, "Label3")
+        Label3.AutoSize = True
+        Label3.Location = New Point(119, 192)
         Label3.Name = "Label3"
+        Label3.Size = New Size(404, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "If you want to run the app when Windows starts, check de checkbox below."
         ' 
         ' Button1
         ' 
-        resources.ApplyResources(Button1, "Button1")
+        Button1.Location = New Point(248, 235)
         Button1.Name = "Button1"
+        Button1.Size = New Size(135, 42)
+        Button1.TabIndex = 4
+        Button1.Text = "Update!"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' AutoStartCheckBox
         ' 
-        resources.ApplyResources(AutoStartCheckBox, "AutoStartCheckBox")
+        AutoStartCheckBox.AutoSize = True
+        AutoStartCheckBox.Location = New Point(196, 292)
         AutoStartCheckBox.Name = "AutoStartCheckBox"
+        AutoStartCheckBox.Size = New Size(245, 19)
+        AutoStartCheckBox.TabIndex = 5
+        AutoStartCheckBox.Text = "Auto update time when Windows® starts"
         AutoStartCheckBox.UseVisualStyleBackColor = True
         ' 
         ' Label4
         ' 
-        resources.ApplyResources(Label4, "Label4")
+        Label4.AutoSize = True
         Label4.BackColor = Color.Transparent
+        Label4.Location = New Point(599, 293)
         Label4.Name = "Label4"
+        Label4.Size = New Size(28, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "v1.0"
+        Label4.TextAlign = ContentAlignment.BottomRight
         ' 
         ' githubLink
         ' 
-        resources.ApplyResources(githubLink, "githubLink")
+        githubLink.AutoSize = True
+        githubLink.Location = New Point(560, 307)
         githubLink.Name = "githubLink"
+        githubLink.Size = New Size(73, 15)
+        githubLink.TabIndex = 9
         githubLink.TabStop = True
+        githubLink.Text = "retrozinndev"
+        githubLink.TextAlign = ContentAlignment.BottomRight
         ' 
         ' githubLogo
         ' 
         githubLogo.Cursor = Cursors.Hand
-        resources.ApplyResources(githubLogo, "githubLogo")
+        githubLogo.Image = CType(resources.GetObject("githubLogo.Image"), Image)
+        githubLogo.Location = New Point(535, 303)
         githubLogo.Name = "githubLogo"
+        githubLogo.Size = New Size(26, 22)
+        githubLogo.SizeMode = PictureBoxSizeMode.Zoom
+        githubLogo.TabIndex = 10
         githubLogo.TabStop = False
+        ' 
+        ' NotifyIcon
+        ' 
+        NotifyIcon.BalloonTipIcon = ToolTipIcon.Info
+        NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), Icon)
+        NotifyIcon.Text = "UpDateNTime"
         ' 
         ' Window
         ' 
-        resources.ApplyResources(Me, "$this")
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
+        ClientSize = New Size(645, 331)
+        Controls.Add(githubLogo)
         Controls.Add(githubLink)
         Controls.Add(Label4)
         Controls.Add(AutoStartCheckBox)
@@ -94,8 +139,12 @@ Partial Class Window
         Controls.Add(Label3)
         Controls.Add(Label2)
         Controls.Add(PictureBox1)
-        Controls.Add(githubLogo)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MaximumSize = New Size(661, 370)
         Name = "Window"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "UpDateN'Time!"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         CType(githubLogo, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
@@ -109,4 +158,5 @@ Partial Class Window
     Friend WithEvents Label4 As Label
     Friend WithEvents githubLink As LinkLabel
     Friend WithEvents githubLogo As PictureBox
+    Friend WithEvents NotifyIcon As NotifyIcon
 End Class
--- UpdateDateNTime/Window.vb
+++ UpdateDateNTime/Window.vb
@@ -1,22 +1,37 @@
-﻿Imports System.IO
-
+﻿
 Public Class Window
-    Dim checkedCheckBox As Boolean
     Dim AutoStartInst As AutoStart = New AutoStart()
 
     Public Sub OpenProfileLink()
         Dim profileURL As String = "https://www.github.com/retrozinndev"
-        Shell("cmd /k start " & profileURL, AppWinStyle.MinimizedNoFocus)
+        Shell("cmd /k start " & profileURL, AppWinStyle.Hide)
     End Sub
     Public Sub UpdateTime()
         Dim fileName As String = "Resources\update.cmd"
         Dim command As String = "cmd /k start " & fileName
-        Shell(command, AppWinStyle.MinimizedNoFocus)
+        Shell(command, AppWinStyle.Hide)
     End Sub
     Private Sub Window_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         UpdateTime()
         If My.Computer.FileSystem.FileExists(AutoStart.GetLinkPath) Then
             AutoStartCheckBox.Checked = True
+        End If
+    End Sub
+    Private Sub Window_LoadFocused(sender As Object, e As EventArgs) Handles MyBase.GotFocus, MyBase.Load
+        ' Start minimized if autostart is on
+        If My.Computer.FileSystem.FileExists(AutoStart.GetLinkPath) Then
+            WindowState = FormWindowState.Normal
+            NotifyIcon.Visible = True
+            Hide()
+        End If
+    End Sub
+
+    Private Sub Window_Minimize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        ' Move app to Task Bar if minimized
+        If WindowState = FormWindowState.Minimized Then
+            NotifyIcon.Visible = True
+            NotifyIcon.ShowBalloonTip(3, "UpDateN'Time", "App minimized to Task bar. Double click in the app icon to restore window!", ToolTipIcon.Info)
+            Hide()
         End If
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -37,5 +52,11 @@ Public Class Window
         Else
             AutoStartInst.RemoveAutoStartLink()
         End If
+    End Sub
+
+    Private Sub NotifyIcon_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon.MouseDoubleClick
+        NotifyIcon.Visible = False
+        Show()
+        WindowState = FormWindowState.Normal
     End Sub
 End Class
